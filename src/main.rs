c18841e6a9c1c3a707152b98a6c41e934f55bf12
mage <tifenn.fl@gmail.com>
2021-12-29 00:10:28 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    let init = Init {
+        max_player: 64,
+        name: "test".to_string(),
+    };
+
+    reqwest::Client::new()
+        .post("http://localhost:3024/tournament")
+        .json(&init)
+        .send()
+        .await
+        .unwrap();
+
+    let ins_state = InsState { open: true };
+
+    reqwest::Client::new()
+        .post("http://localhost:3024/tournament/inscriptions/status")
+        .json(&ins_state)
+        .send()
+        .await
+        .unwrap();
+
+    let mut error_list = Vec::new();
+
+    for n in 0..64 {
+        let player = Player {
+            league_name: n.to_string(),
+            discord_name: "test#4".to_string(),
+        };
+
+        let request = reqwest::Client::new()
+            .post("http://localhost:3024/tournament/inscriptions")
+            .json(&player);
+
+        let res = request.send().await.unwrap();
+
+        if !res.status().is_success() {
+            error_list.push(res.status());
+        }
+    }
+
+    reqwest::get("http://localhost:3024/tournament")
+        .await
+        .unwrap();
+
+    println!("error: {}", error_list.len());
+
+    if error_list.len() > 0 {
+        error_list
+            .iter()
+            .for_each(|err| println!("StatusCode: {}", err));
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Player {
+    league_name: String,
+    discord_name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct InsState {
+    open: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Init {
+    max_player: usize,
+    name: String,
+}
